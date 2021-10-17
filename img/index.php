646fdfd6ca4284dafe11187a5ed3451f0c7b89eb
idna001 <arv001@web.de>
2021-10-17 20:42:51 +0200

    add data class

--- img/index.php
+++ img/index.php
@@ -1,2 +1,21 @@
 <?php
 class index
+{
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        $DB = new DB(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+        $result = $DB->query('SELECT * FROM Data ORDER BY id ASC');
+
+        $data = [];
+        if ($result->num_rows > 0) {
+            while ($obj = $result->fetch_object(get_class($this))) {
+                $data[$obj->anfrgeid] = $obj;
+            }
+        }
+        return $data;
+    }
+}
